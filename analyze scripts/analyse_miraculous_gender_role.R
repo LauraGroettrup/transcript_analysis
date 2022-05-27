5639ac1241fcd36a0e7e700a53f7fb6ba64a93dd
faschmar <martin.faschingbauer@me.com>
2022-05-27 23:42:13 +0200

    update gender/role
    all characters have gender, all villians and kwawis in role;
    merge with dialogtable, linetable and sentimenttable completed

--- analyze scripts/analyse_miraculous_gender_role.R
+++ analyze scripts/analyse_miraculous_gender_role.R
@@ -50,6 +50,37 @@ colnames(character_lookup) <- c("Character", "Gender", "Role")
     group_by(Character, Season) %>%
     summarize(Frequency=n())%>% arrange(desc(Frequency))
   characters_season <-characters_season  %>% left_join(character_lookup, by='Character')
+  
+#show differences between characters_series and lookup
+  #genderna<-characters_series %>% filter(is.na(Gender))
+  #write characters without gender
+    #write.table(genderna,"./data/miraculous/lookup/genderna.csv", row.names = F, append = F, col.names = F, sep = ";")
+    #character_lookup1 <- read.csv("./data/miraculous/lookup/Ohne.csv", sep=";", header = T)
+    #write.table(character_lookup1,"./data/miraculous/lookup/lookup.csv", row.names = F, append = F, col.names = T, sep = "|")
+    
+#gender frequencys
+    characters_series_gender <- characters_series %>% #all characters aus 
+    group_by(Gender) %>%
+    summarize(Frequency=n())%>% arrange(desc(Frequency))
 
+#role frequencys
+  characters_series_role <- characters_series %>% #all characters aus 
+  group_by(Role) %>%
+  summarize(Frequency=n())%>% arrange(desc(Frequency))
+  
+#join gender and role with lineTable
+  lineTable_gender_role<-lineTable %>%left_join(character_lookup, by='Character')
+  
+#join gender and role with dialogTable
+  colnames(character_lookup) <- c("From", "Gender_From", "Role_From") #muss für join lineTable - FROM
+  dialogTable_gender_role<-dialogTable %>%left_join(character_lookup, by='From') 
+  colnames(character_lookup) <- c("To", "Gender_To", "Role_To") #muss für join lineTable - TO
+  dialogTable_gender_role<-dialogTable_gender_role %>%left_join(character_lookup, by='To') 
+  colnames(character_lookup) <- c("Character", "Gender", "Role") # soll für character_lookup
+
+#join gender and role with sentimentTable
+  sentimentTable_gender_role<-sentimentTable %>%left_join(character_lookup, by='Character')
+  
+  
 print("<End of Script>")
 #------------------------------------------------------------------------
