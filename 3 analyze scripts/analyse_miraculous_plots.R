40970eaf962a0f594f758b154913044a634945d3
Laura Gröttrup <laura.groettrup@edu.fh-joanneum.at>
2022-06-18 14:36:46 +0200

    Colorful graph

--- 3 analyze scripts/analyse_miraculous_plots.R
+++ 3 analyze scripts/analyse_miraculous_plots.R
@@ -209,9 +209,11 @@ genderSozioTable %>% #crosstable
 names(gender_top_sentiment_df)[3] <- "weight"
 gender_top_sentiment_df$weight <- round(gender_top_sentiment_df$weight,digit=3)
 gender_top_sentiment_igraph<-graph_from_data_frame(gender_top_sentiment_df)
-igraph.options(plot.layout=layout.circle, vertex.size=25,edge.curved=T, edge.label.y =0, edge.label.x =1.5)
+igraph.options(plot.layout=layout.circle, vertex.size=40,edge.curved=T)
 par(bg = "#f7f7f7")
-plot(simplify(gender_top_sentiment_igraph), edge.label = E(gender_top_sentiment_igraph)$weight, main=paste("Sentiment scores of most central characters and genders"))
+V(gender_top_sentiment_igraph)$color <- c("pink", "#fff0b3", "#99ccff" )
+E(gender_top_sentiment_igraph)$color <- c("pink", "#fff0b3", "#99ccff","pink", "#fff0b3", "#99ccff","pink", "#fff0b3", "#99ccff" )
+plot(gender_top_sentiment_igraph, edge.label = E(gender_top_sentiment_igraph)$weight, main=paste("Sentiment scores of most central characters and genders"))
 par(bg = "white")
 # Female Main to Gender
 importantCharacterList <- character_betweenness[which(character_betweenness$Gender == "female"), ] %>% slice(1:10)
@@ -230,7 +232,8 @@ gender_top_sentiment_df <- aggregate(genderSozioTable$Sentiment, list(genderSozi
 names(gender_top_sentiment_df)[3] <- "weight"
 gender_top_sentiment_df$weight <- round(gender_top_sentiment_df$weight,digit=3)
 gender_top_sentiment_igraph<-graph_from_data_frame(gender_top_sentiment_df)
-igraph.options(plot.layout=layout.circle, vertex.size=25,edge.curved=TRUE, edge.label.y =0, edge.label.x =1.5)
+V(gender_top_sentiment_igraph)$color <- c("pink", "#fff0b3", "#99ccff" )
+E(gender_top_sentiment_igraph)$color <- c("pink", "#fff0b3", "#99ccff","pink", "#fff0b3", "#99ccff","pink", "#fff0b3", "#99ccff" )
 plot(gender_top_sentiment_igraph, edge.label = E(gender_top_sentiment_igraph)$weight, main=paste("Sentiment scores of the 10 most central female characters and genders"))
 # Male Main To Gender
 importantCharacterList <- character_betweenness[which(character_betweenness$Gender == "male"), ] %>% slice(1:10)
@@ -249,7 +252,8 @@ gender_top_sentiment_df <- aggregate(genderSozioTable$Sentiment, list(genderSozi
 names(gender_top_sentiment_df)[3] <- "weight"
 gender_top_sentiment_df$weight <- round(gender_top_sentiment_df$weight,digit=3)
 gender_top_sentiment_igraph<-graph_from_data_frame(gender_top_sentiment_df)
-igraph.options(plot.layout=layout.circle, vertex.size=25,edge.curved=TRUE, edge.label.y =0, edge.label.x =1.5)
+V(gender_top_sentiment_igraph)$color <- c("pink", "#fff0b3", "#99ccff" )
+E(gender_top_sentiment_igraph)$color <- c("pink", "#fff0b3", "#99ccff","pink", "#fff0b3", "#99ccff","pink", "#fff0b3", "#99ccff" )
 plot(gender_top_sentiment_igraph, edge.label = E(gender_top_sentiment_igraph)$weight, main=paste("Sentiment scores of the 10 most central male characters and genders"))
 
 
